abb7f9ad83b7a3a1cab209759f5dc924b56a9186
André <andrerochajp@gmail.com>
2023-09-23 22:07:25 -0300

    notificações e ajuste de layout

--- src/App.js
+++ src/App.js
@@ -4,11 +4,24 @@ import { AiOutlineCopy } from 'react-icons/ai';
 import { useState } from 'react';
 import api from './services/api';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   const [input, setInput] = useState('');
   const [cep, setCep] = useState({});
-  
+
+  function aviso() {
+    toast(
+      "Copiado!",
+      {
+        position: toast.POSITION.BOTTOM_CENTER,
+        autoClose: 500,
+        theme: "dark",
+        hideProgressBar: true,
+      }
+    );
+  }
   async function hs() {
     if (input === '') {
       alert("Preencha o CEP.");
@@ -44,30 +57,43 @@ function App() {
             <h2>{cep.cep}</h2>
             <div className='logradouro'>
               <span>RUA: {cep.logradouro}</span>
-              <CopyToClipboard text={cep.logradouro}>
-                <AiOutlineCopy size={16}></AiOutlineCopy>
-              </CopyToClipboard>
+              <button className='bCopy' onClick={aviso}>
+                <CopyToClipboard text={cep.logradouro}>
+                  <AiOutlineCopy className='cb' size={16}></AiOutlineCopy>
+                </CopyToClipboard>
+                <ToastContainer />
+              </button>
             </div>
 
             <div className='bairro'>
               <span>BAIRRO: {cep.bairro}</span>
-              <CopyToClipboard text={cep.bairro}>
-                <AiOutlineCopy size={16}></AiOutlineCopy>
-              </CopyToClipboard>
+              <button className='bCopy' onClick={aviso}>
+                <CopyToClipboard text={cep.bairro}>
+                  <AiOutlineCopy className='cb' size={16}></AiOutlineCopy>
+                </CopyToClipboard>
+                <ToastContainer />
+              </button>
+
             </div>
 
             <div className='localidade'>
               <span>CIDADE: {cep.localidade}</span>
-              <CopyToClipboard text={cep.localidade}>
-                <AiOutlineCopy size={16}></AiOutlineCopy>
-              </CopyToClipboard>
+              <button className='bCopy' onClick={aviso}>
+                <CopyToClipboard text={cep.localidade}>
+                  <AiOutlineCopy className='cb' size={16}></AiOutlineCopy>
+                </CopyToClipboard>
+                <ToastContainer />
+              </button>
             </div>
 
             <div className='uf'>
               <span>UF: {cep.uf}</span>
-              <CopyToClipboard text={cep.uf}>
-                <AiOutlineCopy size={16}></AiOutlineCopy>
-              </CopyToClipboard>
+              <button className='bCopy' onClick={aviso}>
+                <CopyToClipboard text={cep.uf}>
+                  <AiOutlineCopy className='cb' size={16}></AiOutlineCopy>
+                </CopyToClipboard>
+                <ToastContainer />
+              </button>
             </div>
           </main>
         )}
